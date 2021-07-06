b297654885a211dec0fe90e019b2e2c5506ca813
Gildo Neto <gildorama@gmail.com>
2021-07-06 20:50:11 -0300

    refactor: atualiza bot login

--- app.js
+++ app.js
@@ -2,6 +2,4 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
-bot.login('ODYyMTEzMDk3Njk4NTA4ODAx.YOTnYw.oeGGHLZC_lqFVLcwQK1G6UNlyro');
-
-//branch gildo
+bot.login('ODYyMTEyODQwMjU2NzE2ODEw.YOTnJg.CaUQywZZbu8NbjXP3lto_81wV-8');

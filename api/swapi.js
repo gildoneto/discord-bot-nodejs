ab4001faabece79e8987fb668610ca86e96096b4
Gildo <gildoneto@hotmail.com>
2021-07-09 12:47:42 -0300

    popuulando o aray de planetas com sucesso

--- api/swapi.js
+++ api/swapi.js
@@ -10,16 +10,16 @@ const getSwapi = async () => {
 
     do { let response = await axios.get(`${baseUrl}planets/${page}${num}`)
     const {data} = response
-    elements.push(data.results.map(item => item.name ))
-    console.log(num, elements)
+    data.results.map(item => elements.push(item.name ))
+    //console.log(num, elements)
     next = data.next
     num++
     } while (next != null)
 
-    // console.log(elements)
+    console.log(elements)
 }
 
-const getSwapi2 = async () => {
+const testando = async () => {
     let planets = []
     let num = 1
     let response = await axios.get(`${baseUrl}planets/${page}${num}`)
@@ -27,6 +27,7 @@ const getSwapi2 = async () => {
     data.results.map(item => planets.push(item.name) )
 
     console.log(planets)
+    console.log(planets[0])
 }
 
-getSwapi2()
+getSwapi()

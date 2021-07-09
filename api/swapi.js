14d47a3b977c21e63e725411c373f8631e48392e
Gildo <gildoneto@hotmail.com>
2021-07-09 12:21:48 -0300

    Da push nos elements, mas cria varios arrays

--- api/swapi.js
+++ api/swapi.js
@@ -2,20 +2,21 @@ const axios = require('axios')
 const { MessageEmbed } = require('discord.js')
 const baseUrl = 'https://swapi.dev/api/'
 const page = `?page=`
+let elements = []
 
 const  getSwapi = async () => {
     let num = 1
     let next
-    const elements = []
 
-    do {let response = await axios.get(`${baseUrl}planets/${page}${num}`)
-    console.log(num, response.data.name, response.data.next)
-    elements.push(response.data.name)
-    next = response.data.next
+    do { let response = await axios.get(`${baseUrl}planets/${page}${num}`)
+    const {data} = response
+    elements.push(data.results.map(item => item.name ))
+    console.log(num, elements)
+    next = data.next
     num++
     } while (next != null)
 
-    console.log(elements)
+    // console.log(elements)
 }
 
 getSwapi()

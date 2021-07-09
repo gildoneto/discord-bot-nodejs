23cfeb08914059231c9f6b0bdd809f81ba1408ab
Gildo <gildoneto@hotmail.com>
2021-07-09 16:33:25 -0300

    criacao de arquivos

--- api/swapi.js
+++ api/swapi.js
@@ -3,28 +3,50 @@ const { MessageEmbed } = require('discord.js')
 const baseUrl = 'https://swapi.dev/api/'
 const page = '/?page='
 
-const getSwapi = async (param) => {
-    let num = 1
+const getAllPages = async (channel, param) => {
+    let pageNumber = 1
     let next
     const elements = []
 
-    do { let response = await axios.get(getUrl(param, num))
-    const {data} = response
-    data.results.map(item => elements.push(item.name ))
-    //console.log(num, elements)
-    next = data.next
-    num++
+    do { let response = await axios.get(getUrl(param, pageNumber))
+        const {data} = response
+        data.results.map(item => elements.push(item.name ))
+        next = data.next
+        pageNumber++
     } while (next != null)
 
     console.log(elements)
 }
 
-const getUrl = (endpoint, num) => { 
-    return `${baseUrl}${endpoint}${page}${num}`
+const getPage = async (channel, param, pageNumber) => {
+    const elements = []
+
+    const response = await axios.get(getUrl(param, pageNumber))
+    const {data} = response
+    data.results.map(item => elements.push(item.name ))
+    console.log(elements)
+
+    channel.send(new MessageEmbed()
+    .setDescription(elements.map(element => `${element.name}`))
+    )
 }
 
-//getSwapi('people')
-//getSwapi('planets')
-//getSwapi('starships')
-// getSwapi('species')
-getSwapi('vehicles')
+const getUrl = (endpoint, pageNumber) => { 
+    return `${baseUrl}${endpoint}${page}${pageNumber}`
+    
+}
+
+//getAllPages('people')
+//getAllPages('planets')
+//getAllPages('starships')
+// getAllPages('species')
+// getAllPages('vehicles')
+// getPage('people', 3)
+
+const getPeople = async (channel, pageNumber) => await getPage(channel, 'people', pageNumber)
+const getPlanets = async (channel, pageNumber) => await getPage(channel, 'planets', pageNumber)
+const getSpecies = async (channel, pageNumber) => await getPage(channel, 'species', pageNumber)
+const getVehicle = async (channel, pageNumber) => await getPage(channel, 'vehicles', pageNumber)
+const getStarship = async (channel, pageNumber) => await getPage(channel, 'starships', pageNumber)
+
+module.exports = {getAllPages, getPage}

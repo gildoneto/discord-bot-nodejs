5f99f0237e6f1f7612e9e20b848e592526a5239b
Gildo <gildoneto@hotmail.com>
2021-07-15 19:55:56 -0300

    primeiro deploy

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const { Client } = require('discord.js')
 const discordController = require('./controller/discord')
 const variables = require('./db/variables.json')
+require('dotenv/config')
 
 new Client()
   .on('message', discordController.getMessage)
-  .login(variables.discordKey)
+  .login(process.env.DISCORD_KEY)
